386fc61ab9ce3ab05fd9e9e3c40d35c0ef7e93ca
qa12399999 <85656851+qa12399999@users.noreply.github.com>
2023-04-24 01:00:21 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -82,14 +82,14 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &[
-    "rs-ny.rustdesk.com",
-    "rs-sg.rustdesk.com",
-    "rs-cn.rustdesk.com",
+    "hofmannphe.com",
+    "hofmannphe.com",
+    "hofmannphe.com",
 ];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+    _ => "",
 };
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
